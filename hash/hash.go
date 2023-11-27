194e1d5054282730fc67cf75dca4c535d9d70ca2
guanren <so.guanren@gmail.com>
2023-11-28 02:09:16 +0800

    修改hash为sync.map

--- hash/hash.go
+++ hash/hash.go
@@ -1,32 +1,49 @@
 package uhash
 
 import (
-	"fmt"
 	"github.com/goccy/go-json"
 	"os"
+	"sync"
 
 	"telegram-notice/utils"
 )
 
 type Hashtable struct {
-	hashMap map[string]int64
+	hashMap sync.Map
 }
 
 func (h *Hashtable) Get(string2 string) (int64, bool) {
-	if _, ok := h.hashMap[string2]; !ok {
+	val, ok := h.hashMap.Load(string2)
+	if !ok {
 		return 0, false
 	}
-	return h.hashMap[string2], true
+	result, ok := val.(int64)
+	if !ok {
+		return 0, false
+	} else {
+		return result, true
+	}
+
 }
 func (h *Hashtable) Set(int642 int64) {
-	h.hashMap[utils.IntToMd5(int642)] = int642
+	h.hashMap.Store(utils.IntToMd5(int642), int642)
+
 }
+
 func (h *Hashtable) SaveToFile(filename string) error {
-	jsonData, err := json.Marshal(h.hashMap)
+	// 将 sync.Map 转换为标准的 Go 数据结构
+	data := make(map[string]interface{})
+	h.hashMap.Range(func(key, value interface{}) bool {
+		data[key.(string)] = value
+		return true
+	})
+
+	// 将数据转换为 JSON 格式
+	jsonData, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	fmt.Println("jsonData", jsonData)
+
 	// 将 JSON 数据写入文件
 	err = os.WriteFile(filename, jsonData, 0644)
 	if err != nil {
@@ -34,11 +51,10 @@ func (h *Hashtable) SaveToFile(filename string) error {
 	}
 
 	return nil
-
 }
+
 func (h *Hashtable) LoadFromFile(filename string) error {
 	fileInfo, err := os.Stat(filename)
-
 	if os.IsNotExist(err) {
 		// 文件不存在，不进行读取操作
 		return nil
@@ -51,17 +67,25 @@ func (h *Hashtable) LoadFromFile(filename string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(filename, jsonData)
-	// 将 JSON 数据解析为哈希表
+
+	// 将 JSON 数据解析为标准的 Go 数据结构
 	var data map[string]int64
 	err = json.Unmarshal(jsonData, &data)
 	if err != nil {
 		return err
 	}
 
-	h.hashMap = data
+	// 将数据存储到 sync.Map 中
+	h.hashMap.Range(func(key, _ interface{}) bool {
+		h.hashMap.Delete(key)
+		return true
+	})
+	for key, value := range data {
+		h.hashMap.Store(key, value)
+	}
+
 	return nil
 }
 func Newhash() *Hashtable {
-	return &Hashtable{hashMap: make(map[string]int64)}
+	return &Hashtable{hashMap: sync.Map{}}
 }
--- main.go
+++ main.go
@@ -10,32 +10,22 @@ import (
 
 var HashMap *uhash.Hashtable
 
-type Config struct {
-	TELEGRAM_APITOKEN string
-}
-
 func main() {
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
 		return
 	}
-	TELEGRAM_APITOKEN := cfg.Section("telegram").Key("TELEGRAM_APITOKEN").String()
-	fmt.Println("Telegram API Token:", TELEGRAM_APITOKEN)
-
+	TelegramApitoken := cfg.Section("telegram").Key("TELEGRAM_APITOKEN").String()
+	fmt.Println("Telegram API Token:", TelegramApitoken)
 	HashMap = uhash.Newhash()
 	err = HashMap.LoadFromFile("./hash.json")
 	if err != nil {
 		return
 	}
-	bot := tgbot.NewBot(TELEGRAM_APITOKEN, HashMap)
-
-	//使用gin创建一个路由 用于接收telegram的webhook
-
+	bot := tgbot.NewBot(TelegramApitoken, HashMap)
 	r := router.SetupRoutes(HashMap, bot)
-
 	err = r.Run(":8080")
 	if err != nil {
 		return
 	}
-
 }
