041acfe78b785321cf4fd52530a7380091f235f7
guanren <so.guanren@gmail.com>
2023-12-16 03:36:37 +0800

    修改目录结构
    Signed-off-by: guanren <so.guanren@gmail.com>

--- main.go
+++ main.go
@@ -11,20 +11,20 @@ import (
 var HashMap *uhash.Hashtable
 
 func main() {
-	cfg, err := ini.Load("config.ini")
+	cfg, err := ini.Load("config/config.ini")
 	if err != nil {
 		return
 	}
 	TelegramApitoken := cfg.Section("telegram").Key("TELEGRAM_APITOKEN").String()
 	fmt.Println("Telegram API Token:", TelegramApitoken)
 	HashMap = uhash.Newhash()
-	err = HashMap.LoadFromFile("./hash.json")
+	err = HashMap.LoadFromFile("config/hash.json")
 	if err != nil {
 		return
 	}
 	bot := tgbot.NewBot(TelegramApitoken, HashMap)
 	r := router.SetupRoutes(HashMap, bot)
-	err = r.Run(":8080")
+	err = r.Run(":2095")
 	if err != nil {
 		return
 	}
